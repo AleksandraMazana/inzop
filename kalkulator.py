7b96faf8f00122d2b4d1546c4ebb32e44cfa00f3
Ola <aleksndra.mazana@gmail.com>
2018-09-12 11:13:05 +0200

    kalkulator w pythonie

---
+++ kalkulator.py
@@ -0,0 +1,15 @@
+# def pogadaj_ze_mna():
+#     print("Hej, jak ci na imie?")
+#     imie = input()
+#     print("Siema "+imie+"!")
+#     return imie
+#
+# def zrob_awanture(imie_zdrajcy):
+#     print("Gosia dużo rozmawiasz z " + imie_osoby)
+#
+# imie_typa = pogadaj_ze_mna()
+# zrob_awanture(imie_typa)
+zakres_liczb = range(10)
+liczby_pierwsze = [1,2,3,5,7,11,13]
+for i in liczby_pierwsze:
+    print(str(i))
