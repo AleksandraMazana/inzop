878d2008a79398eb87ced03961bf3ebc42b2b8c4
Ola <aleksndra.mazana@gmail.com>
2018-09-12 11:33:21 +0200

    ostateczne zmiany

--- kalkulator.py
+++ kalkulator.py
@@ -1,20 +1,3 @@
-# def pogadaj_ze_mna():
-#     print("Hej, jak się masz?")
-#     imie = input()
-#     print("Siema "+imie+"!")
-#     return imie
-#
-# def odpowiedz(imie_kolegi):
-#     print("Świetnie, a Ty " + imie_osoby)
-#
-# imie_typa = pogadaj_ze_mna()
-# odpowiedz(imie_typa)
-zakres_liczb = range(10)
-liczby_pierwsze = [1,2,3,5,7,11,13]
-for i in liczby_pierwsze:
-    print(str(i))
-
-
 import string
 
 liczba_dokumentow = int(input())
