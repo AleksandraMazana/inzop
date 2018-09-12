b660135d58f15bf377ce8b9eb640a1c94ac0c24c
Ola <aleksndra.mazana@gmail.com>
2018-09-12 11:23:27 +0200

    wprowadzam pierwszą zmianę

--- kalkulator.py
+++ kalkulator.py
@@ -1,14 +1,14 @@
 # def pogadaj_ze_mna():
-#     print("Hej, jak ci na imie?")
+#     print("Hej, jak się masz?")
 #     imie = input()
 #     print("Siema "+imie+"!")
 #     return imie
 #
-# def zrob_awanture(imie_zdrajcy):
-#     print("Gosia dużo rozmawiasz z " + imie_osoby)
+# def odpowiedz(imie_kolegi):
+#     print("Świetnie, a Ty " + imie_osoby)
 #
 # imie_typa = pogadaj_ze_mna()
-# zrob_awanture(imie_typa)
+# odpowiedz(imie_typa)
 zakres_liczb = range(10)
 liczby_pierwsze = [1,2,3,5,7,11,13]
 for i in liczby_pierwsze:
